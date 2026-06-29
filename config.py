56a57887265b846c2209df2d09cf96e3195fc923
sardor_bloog <sardorbeksobirjonov308@gmail.com>
2026-06-29 07:08:07 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+# Bot tokenini shu yerga kiriting (BotFather'dan olingan)
+BOT_TOKEN = "8485762290:AAECB_dUuXbgDaIub-dKxFWNd9cFAR7IFBw"
+
+# Admin paneli uchun maxsus parol
+ADMIN_PASSWORD = "superadmin123"
+
+# Adminlarning Telegram ID raqamlari
+ADMINS = [123456789] 
+
+# Telegram Web App uchun URL (Sizning yangi serveringiz)
+WEBAPP_URL = "https://sdr.yangi-jadid.uz/"
+
+# Ma'lumotlar bazasi parametrlari
+DB_HOST = "localhost"
+DB_USER = "db1"
+DB_PASSWORD = "sardorproject"
+DB_NAME = "db1"
